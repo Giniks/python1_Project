835cdba972897d037f1776e2da8dba6e5e4958c4
Ginika Elizabeth Nna <elizabethginika9@gmail.com>
2024-03-02 07:47:00 +0100

    treasure_hunt

---
+++ 0x02-treasure_hunting/README.md
@@ -0,0 +1,75 @@
+## Project Title:  Treasure Hunting, Live or Die!
+
+Welcome to the Treasure Hunting, Live or Die game! This is an exciting text-based adventure where your choices determine whether you find treasure or meet a perilous fate.
+
+## Welcoming Message
+
+Get ready for an immersive adventure! In this game, you'll navigate through challenging scenarios, make crucial decisions, and see where the path leads you.
+
+## Project Overview
+
+Treasure Hunting, Live or Die is a Python text-based adventure game. Players make choices at various points in the game, leading to different outcomes. The goal is to find the treasure without meeting a deadly end.
+
+## Table of Contents
+
+1. Welcoming Message
+2. Project Overview
+3. How to Play
+4. Installation
+5. Contribution Guidelines
+6. Acknowledgements
+7. Author
+
+## How to Play
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Run the `treasure_game.py` script using Python.
+
+## Installation
+
+To install the game locally, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/treasure-hunting-live-or-die.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd 0x02-treasure-hunting
+   ```
+3. Run the game script:
+   ```bash
+   python treasure_hunt.py
+   ```
+
+## What I Want Viewers to Perceive
+
+I want players to experience an engaging adventure where their choices have consequences. The game should provide excitement, challenge, and enjoyment.
+
+## Acknowledgements
+
+Special thanks to my bootcamp "100 days of python coding"for providing valuable insights for this project.
+## Author
+
+- Name: Ginika Elizabeth
+- Email: elizabethginika9@gmail.com
+
+## Contribution Guidelines
+
+Contributions are welcome! If you find any bugs or have suggestions for improvement, please open an issue or submit a pull request.
+
+## Note
+
+There is a better version of this code for proper comprehension: 'better_version.py
+
+## Execute the Python File
+
+To start playing the game, execute the Python file `treasure_hunt.py` and 'better_version.py'  using Python.
+
+```bash
+python treasure_hunt.py
+```
+
+Feel free to explore, provide feedback, and collaborate on this project. Let's make the adventure even better together!
+
---
+++ 0x02-treasure_hunting/treasure_hunt.py
@@ -0,0 +1,56 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Hunting Island, Live or Die!.")
+print("Your mission is to find the treasure, so you can Live or Die when you don't find it.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("You are at a crossroad with no GPS or Map, oneside is a tropical rainforest, the other an endless ocean;")
+choice1 = input("Where do you want to go? Type right or left: ").lower()
+if choice1 == 'left':
+  print("Welcome to a tropical rainforest, you are now lost forever, Game Over!")
+  exit()
+elif choice1 == 'right':
+  choice2 = input("Welcome to an endless ocean, you see a boat in the distance, do you want to wait or swim? ").lower()
+  if choice2 == 'wait':
+    print("You chose wait, let's see what happens next!")
+    print("Here is your boat! Off you go!")
+    choice3 = input("Welcome to the island, you see a house with 3 doors, a red one, a blue one and a yellow one, which one do you choose? ").lower()
+    if choice3 == 'red':
+      print("You chose the red door, you are burned by fire, Game Over!")
+      exit()
+    elif choice3 == 'blue':
+      print("You chose the blue door, you are eaten by a beast, Game Over!")
+      exit()
+    elif choice3 == 'yellow':
+      print("Congratulations!, You have found the Treasure, Skilled Hunter! You win!")
+      exit()
+    else:
+      print("invalid choice, try again, which colour from the 3 door do you choose? ")
+  elif choice2 == 'swim':
+    print("You have been eaten by a gigantic Shark, Game Over!")
+  else:
+    print("Invalid input, try again; Please enter 'wait' or 'swim' ")
+else:
+  print("Invalid input, try again; Please enter 'right' or 'left' ")
